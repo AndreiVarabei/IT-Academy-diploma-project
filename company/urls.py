af0a938dd467e6106ff51ae7d5c59ccbdb237af8
Varabei Andrei <varabei1393@gmail.com>
2022-03-31 12:10:51 +0300

    Itaration 5. Add logout

--- company/urls.py
+++ company/urls.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'company'
 urlpatterns = [
     path('', views.all_companies, name='all_companies'),
     path('update', views.update_companies, name='update_companies'),
-    path('login/', views.custom_login, name='login')
+    # path('login/', views.custom_login, name='login'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout')
+
 
 ]
--- finish/settings.py
+++ finish/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'company.apps.CompanyConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'company.apps.CompanyConfig',
 ]
 
 MIDDLEWARE = [
@@ -119,6 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+LOGIN_REDIRECT_URL = '/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
